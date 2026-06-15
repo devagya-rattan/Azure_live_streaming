480f80df11af7c5a47a2e9e5ccf7cb0b0d467372
Devagya Rattan <rattandevagya@gmail.com>
2026-06-16 03:32:05 +0530

    Rtmp server

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(
 );
 
 app.get("/api/live-streaming", (req, res) => {
-  res.json({ url: "http://20.213.156.xx:xxxx/hls/stream.m3u8" });
+  res.json({ url: "http://20.213.156.33:8080/hls/stream.m3u8" });
 });
 
 app.listen(PORT, () => {
